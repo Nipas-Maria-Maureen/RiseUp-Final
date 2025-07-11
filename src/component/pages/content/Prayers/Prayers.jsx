97eb1c84e7f2804a5eb405968f109c39decfb19f
Nipas-Maria-Maureen <147904240+Nipas-Maria-Maureen@users.noreply.github.com>
2025-07-11 10:37:07 +0800

    Update Prayers.jsx

--- src/component/pages/content/Prayers/Prayers.jsx
+++ src/component/pages/content/Prayers/Prayers.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../../../../../frontend/backend/config/firebase';
+import { db } from '../../../../../backend/config/firebase';
 
 export default function Prayers() {
   const [prayerURLs, setPrayerURLs] = useState([]);
